d7c18f3b03a92f6e83e7d22379a75a58f3565574
Konrad Wienecke <konrad.wienecke@mac.com>
2023-12-03 12:34:30 +0100

    Refactor day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,21 +1,5 @@
 use std::{collections::HashMap, fs};
 
-use once_cell::sync::Lazy;
-
-static DIGITS: Lazy<HashMap<&str, char>> = Lazy::new(|| {
-    return HashMap::from([
-        ("one", '1'),
-        ("two", '2'),
-        ("three", '3'),
-        ("four", '4'),
-        ("five", '5'),
-        ("six", '6'),
-        ("seven", '7'),
-        ("eight", '8'),
-        ("nine", '9'),
-    ]);
-});
-
 fn main() {
     let day = 1;
     let input = fs::read_to_string(format!("data/day{:02}.txt", day)).unwrap();
@@ -23,62 +7,56 @@ fn main() {
     println!("Day {}, part 2: {}", day, part_2(&input));
 }
 
-fn concat_first_last(digits: &mut dyn Iterator<Item = char>) -> Option<u32> {
-    let first = digits.next();
-    let last = digits.last(); // is None if there is only one digit in the line
-    let mut first_last = String::with_capacity(2);
-    match (first, last) {
-        (Some(a), Some(b)) => {
-            first_last.push(a);
-            first_last.push(b);
-        }
-        // There might only be one digit. In that case, first and last are the same:
-        (Some(a), None) => {
-            first_last.push(a);
-            first_last.push(a);
-        }
-        _ => {
-            return None;
-        }
-    };
-    return Some(
-        first_last
-            .parse()
-            .unwrap_or_else(|_| panic!("Could not parse number {}", first_last)),
-    );
-}
-
-fn parse_line_1(line: &str) -> u32 {
-    let mut digits = line.chars().filter(|char| char.is_ascii_digit());
-    return concat_first_last(&mut digits)
-        .unwrap_or_else(|| panic!("Cold not parse line {}", line));
-}
-
-fn parse_line_2(line: &str) -> u32 {
-    let mut digits = line.chars().enumerate().filter_map(|(i, c)| {
-        // First, check if we have a simple digit:
-        if c.is_ascii_digit() {
-            return Some(c);
-        }
-        // Otherwise, check if a digit name starts at this position:
-        for &name in DIGITS.keys() {
-            let name_starts_here = line
-                .get(i..i + name.len())
-                .is_some_and(|sub| sub == name);
-            if name_starts_here {
-                return DIGITS.get(name).copied();
-            }
-        }
-        return None;
-    });
-    return concat_first_last(&mut digits)
-        .unwrap_or_else(|| panic!("Could not parse line {}", line));
-}
-
 fn part_1(input: &str) -> u32 {
-    return input.lines().map(|line| parse_line_1(line)).sum();
+    return input
+        .lines()
+        .map(|line| {
+            let line = line;
+            let digits: Vec<u32> = line.chars().filter_map(|c| char::to_digit(c, 10)).collect();
+            assert!(digits.len() > 0, "Could not find any digits in {}", line);
+            return digits[0] * 10 + digits[digits.len() - 1];
+        })
+        .sum();
 }
 
 fn part_2(input: &str) -> u32 {
-    return input.lines().map(|line| parse_line_2(line)).sum();
+    return input
+        .lines()
+        .map(|line| {
+            let line = line;
+            let digit_names = HashMap::from([
+                ("one", 1),
+                ("two", 2),
+                ("three", 3),
+                ("four", 4),
+                ("five", 5),
+                ("six", 6),
+                ("seven", 7),
+                ("eight", 8),
+                ("nine", 9),
+            ]);
+
+            let digits: Vec<u32> = line
+                .chars()
+                .enumerate()
+                .filter_map(|(i, c)| {
+                    // First, check if we have a simple digit:
+                    if c.is_ascii_digit() {
+                        return char::to_digit(c, 10);
+                    }
+                    // Otherwise, check if a digit name starts at this position:
+                    for &name in digit_names.keys() {
+                        let name_starts_here =
+                            line.get(i..i + name.len()).is_some_and(|sub| sub == name);
+                        if name_starts_here {
+                            return digit_names.get(name).copied();
+                        }
+                    }
+                    return None;
+                })
+                .collect();
+            assert!(digits.len() > 0, "Could not find any digits in {}", line);
+            return digits[0] * 10 + digits[digits.len() - 1];
+        })
+        .sum();
 }
